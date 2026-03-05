4281f46baee68b50bf10d2cbb4c918d70651d4b7
Viktor De Pasquale <hello@depasquale.wiki>
2026-03-05 15:29:37 +0100

    chore(build): centralize Kotlin Multiplatform targets configuration
    - Moved iOS, tvOS, watchOS, Linux, macOS, JS, WASM, and Windows targets to `kotlin.multiplatform.gradle.kts`.
    - Removed redundant platform configurations from `superposition/build.gradle.kts`.

--- buildSrc/src/main/kotlin/superposition/kotlin.multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/superposition/kotlin.multiplatform.gradle.kts
@@ -9,6 +9,40 @@ plugins {
 
 kotlin {
     jvmToolchain(17)
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    tvosX64()
+    tvosArm64()
+    tvosSimulatorArm64()
+
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    watchosSimulatorArm64()
+
+    linuxX64()
+    linuxArm64()
+
+    macosX64()
+    macosArm64()
+
+    js {
+        browser()
+        nodejs()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+    }
+    wasmWasi {
+        nodejs()
+    }
+
+    mingwX64()
 }
 
 tasks.withType<Test>().configureEach {
--- superposition/build.gradle.kts
+++ superposition/build.gradle.kts
@@ -3,41 +3,6 @@ plugins {
 }
 
 kotlin {
-
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    tvosX64()
-    tvosArm64()
-    tvosSimulatorArm64()
-
-    watchosX64()
-    watchosArm32()
-    watchosArm64()
-    watchosSimulatorArm64()
-
-    linuxX64()
-    linuxArm64()
-
-    macosX64()
-    macosArm64()
-
-    js {
-        browser()
-        nodejs()
-    }
-    wasmJs {
-        browser()
-        nodejs()
-        d8()
-    }
-    wasmWasi {
-        nodejs()
-    }
-
-    mingwX64()
-
     sourceSets {
         commonMain.dependencies {
         }
