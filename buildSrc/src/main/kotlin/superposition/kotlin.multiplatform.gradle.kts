f1b1f347a36b000ad92ec1283dec6f3eb97ebaab
Viktor De Pasquale <hello@depasquale.wiki>
2026-03-25 11:03:42 +0100

    chore(kotlin): remove deprecated Kotlin Multiplatform targets
    - Removed `tvosX64`, `watchosX64`, and `macosX64` targets.
    - Streamlined configuration by focusing on active targets in the project.

--- buildSrc/src/main/kotlin/superposition/kotlin.multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/superposition/kotlin.multiplatform.gradle.kts
@@ -34,11 +34,9 @@ kotlin {
     iosArm64()
     iosSimulatorArm64()
 
-    tvosX64()
     tvosArm64()
     tvosSimulatorArm64()
 
-    watchosX64()
     watchosArm32()
     watchosArm64()
     watchosSimulatorArm64()
@@ -46,7 +44,6 @@ kotlin {
     linuxX64()
     linuxArm64()
 
-    macosX64()
     macosArm64()
 
     js {
